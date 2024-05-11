3b520c3d32e064447dcc881816097c8d7aec42ab
ladim Og <ladim1703@gmail.com>
2024-05-11 12:14:25 +0200

    fatorisation de la class player et Encounter

--- index.php
+++ index.php
@@ -3,6 +3,27 @@
 // declare(strict_type=1);
 
 
+class Player
+{
+    private int $level;
+
+    public function __construct(int $level)
+    {
+
+        $this->level = $level;
+    }
+
+    public function getLevel(): int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): void
+    {
+        $this->level = $level;
+    }
+}
+
 class Encounter
 {
     public const RESULT_WINNER = 1;
@@ -11,26 +32,26 @@ class Encounter
     public const RESULT_POSSIBILITIES = [self::RESULT_WINNER, self::RESULT_LOSER, self::RESULT_DRAW];
 
 
-    public static function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo)
+    public static function probabilityAgainst(Player $levelPlayerOne, Player $againstLevelPlayerTwo)
     {
-        return 1 / (1 + (10 ** (($againstLevelPlayerTwo - $levelPlayerOne) / 400)));
+        return 1 / (1 + (10 ** (($againstLevelPlayerTwo->getLevel() - $levelPlayerOne->getLevel()) / 400)));
     }
 
-    public static function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
+    public static function setNewLevel(Player $levelPlayerOne, Player $againstLevelPlayerTwo, int $playerOneResult)
     {
         if (!in_array($playerOneResult, self::RESULT_POSSIBILITIES)) {
             trigger_error(sprintf('Invalid result. Expected %s', implode(' or ', self::RESULT_POSSIBILITIES)));
         }
 
-        $levelPlayerOne += (int) (32 * ($playerOneResult - self::probabilityAgainst($levelPlayerOne, $againstLevelPlayerTwo)));
+        $levelPlayerOne->setLevel(
+            $levelPlayerOne->getLevel() + round(32 * ($playerOneResult - self::probabilityAgainst($levelPlayerOne, $againstLevelPlayerTwo)))
+        );
     }
 }
 
 
-$encounter = new Encounter();
-
-$greg = 400;
-$jade = 800;
+$greg = new Player(400);
+$jade = new Player(800);
 
 echo sprintf(
     'Greg à %.2f%% chance de gagner face a Jade',
@@ -38,13 +59,16 @@ echo sprintf(
 ) . PHP_EOL;
 
 // Imaginons que greg l'emporte tout de même.
+
+
+
 Encounter::setNewLevel($greg, $jade, Encounter::RESULT_WINNER);
 Encounter::setNewLevel($jade, $greg, Encounter::RESULT_LOSER);
 
 echo sprintf(
     'les niveaux des joueurs ont évolués vers %s pour Greg et %s pour Jade',
-    $greg,
-    $jade
+    $greg->getLevel(),
+    $jade->getLevel()
 );
 
 exit(0);
