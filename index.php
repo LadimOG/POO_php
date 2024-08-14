4b07fba68ce2c5d304a6adb1ac169a6968578f2c
Ladim <nimirf-dimitri@hotmail.fr>
2024-08-14 19:08:50 +0200

    modification de la methode setNewLevel et l'encapsulation de la class Player

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ class Encounter
 
     public static function probabilityAgainst(Player $levelPlayerOne, Player $levelPlayerTwo)
     {
-        return 1/(1+(10 ** (($levelPlayerTwo->level - $levelPlayerOne->level)/400)));
+        return 1/(1+(10 ** (($levelPlayerTwo->getLevel() - $levelPlayerOne->getLevel())/400)));
     }
 
     public static function setNewLevel(Player $levelPlayerOne, Player $levelPlayerTwo, int $playerOneResult)
@@ -21,16 +21,27 @@ class Encounter
             trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', self::RESULT_POSSIBILITIES)));
         }
 
-        $levelPlayerOne->level += (int) (32 * ($playerOneResult - self::probabilityAgainst($levelPlayerOne, $levelPlayerTwo))); 
+        $currentLevel = $levelPlayerOne->getLevel();
+        $newValue = (int) (32 * ($playerOneResult - self::probabilityAgainst($levelPlayerOne, $levelPlayerTwo))); 
+        $currentLevel += $newValue;
+        $levelPlayerOne->setLevel($currentLevel);
     }
-
-
 }
 
 class Player
 {
-    public int $level;
+    private int $level;
 
+
+    public function setLevel(int $level):void
+    {
+        $this->level = $level;
+    }
+
+    public function getLevel():int
+    {
+        return $this->level;
+    }
 }
 
 $encount = new Encounter;
@@ -39,8 +50,8 @@ $encount = new Encounter;
 $greg = new Player;
 $jade = new Player;
 
-$greg->level = 400;
-$jade->level = 800;
+$greg->setLevel(400);
+$jade->setLevel(800);
 
 
 echo sprintf(
@@ -54,8 +65,8 @@ Encounter::setNewLevel($jade, $greg, Encounter::RESULT_LOSER);
 
 echo sprintf(
     'les niveaux des joueurs ont évolués vers %s pour Greg et %s pour Jade',
-    $greg->level,
-    $jade->level
+    $greg->getLevel(),
+    $jade->getLevel()
 );
 
 exit(0);
