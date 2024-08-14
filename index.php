e50effd914f2173b93f77bac427afdcc59dd9789
Ladim <nimirf-dimitri@hotmail.fr>
2024-08-14 03:06:47 +0200

    ajout des methodes static

--- index.php
+++ index.php
@@ -9,19 +9,19 @@ class Encounter
     const RESULT_DRAW = 0;
     const RESULT_POSSIBILITIES = [self::RESULT_WINNER, self::RESULT_LOSER, self::RESULT_DRAW];
 
-    function probabilityAgainst(Player $levelPlayerOne, Player $levelPlayerTwo)
+    public static function probabilityAgainst(Player $levelPlayerOne, Player $levelPlayerTwo)
     {
         return 1/(1+(10 ** (($levelPlayerTwo->level - $levelPlayerOne->level)/400)));
     }
 
-    function setNewLevel(Player $levelPlayerOne, Player $levelPlayerTwo, int $playerOneResult)
+    public static function setNewLevel(Player $levelPlayerOne, Player $levelPlayerTwo, int $playerOneResult)
     {
         if (!in_array($playerOneResult, self::RESULT_POSSIBILITIES)) 
         {
             trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', self::RESULT_POSSIBILITIES)));
         }
 
-        $levelPlayerOne->level += (int) (32 * ($playerOneResult - $this->probabilityAgainst($levelPlayerOne, $levelPlayerTwo))); 
+        $levelPlayerOne->level += (int) (32 * ($playerOneResult - self::probabilityAgainst($levelPlayerOne, $levelPlayerTwo))); 
     }
 
 
@@ -45,12 +45,12 @@ $jade->level = 800;
 
 echo sprintf(
     'Greg à %.2f%% chance de gagner face a Jade',
-    $encount->probabilityAgainst($greg, $jade)*100
+    Encounter::probabilityAgainst($greg, $jade)*100
 ).PHP_EOL;
 
 // Imaginons que greg l'emporte tout de même.
-$encount->setNewLevel($greg, $jade, Encounter::RESULT_WINNER);
-$encount->setNewLevel($jade, $greg, Encounter::RESULT_LOSER);
+Encounter::setNewLevel($greg, $jade, Encounter::RESULT_WINNER);
+Encounter::setNewLevel($jade, $greg, Encounter::RESULT_LOSER);
 
 echo sprintf(
     'les niveaux des joueurs ont évolués vers %s pour Greg et %s pour Jade',
