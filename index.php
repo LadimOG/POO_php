1a0c9200ad90e2a2c3631351da2e3c0a50c73cb9
Ladim <nimirf-dimitri@hotmail.fr>
2024-08-21 01:02:06 +0200

    amelioration du pproject avec espace de nom et autoloader

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
 declare(strict_types=1);
 
-require_once "./Class/Player.php";
-require_once "./Class/Lobby.php";
-require_once "./Class/BlitzPlayer.php";
+spl_autoload_register(function($class){
+    $path = str_replace(['App', '\\'],['src', '/'], $class.'.php');
+
+    if(file_exists($path)){
+        require_once $path;
+    }
+});
+
+
+use App\MatchMaker\Player\BlitzPlayer;
+use App\MatchMaker\Player\Player;
+use App\MatchMaker\Lobby;
 
 $greg = new Player('greg', 400);
 $jade = new Player('jade', 476);
@@ -13,5 +22,5 @@ $lobby->addPlayers($greg, $jade);
 
 $blitz = new BlitzPlayer();
 
-var_dump($blitz);
-// var_dump($lobby->findOponents($lobby->queuingPlayers[0]));
+var_dump($blitz->fqcn(). PHP_EOL);
+var_dump($lobby->findOponents($lobby->queuingPlayers[0]));
